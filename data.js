035d830a9229985aad3d00db8ca2a54d35d83cae
cespedesjuanc <cespedesjuanc@MacBook-Pro-de-Juan.local>
2022-09-09 22:54:23 -0300

    se modifica data.js

---
+++ data.js
@@ -0,0 +1,95 @@
+const productsData = [
+    {
+        id: 1,
+        name: 'La Mr. Pit',
+        precio: 350,
+        subName:'Solo para expertos',
+        category:'pizza',
+        cardImg:'./img/populares/Photo Menu-4.png',
+    },
+    {
+        id: 2,
+        name: '¡Q`Jamone!',
+        precio: 350,
+        subName:'c/jamón crudo',
+        category:'pizza',
+        cardImg:'img/populares/Photo Menu-1.png',
+    },
+       {
+        id: 3,
+        name:'La Charly García',
+        precio: 380,
+        subName:'BASTA',
+        category:'pizza',
+        cardImg: 'img/populares/Photo Menu-2.png',
+       },
+    
+       {
+        id: 4,
+        name:'La Maradona',
+        precio: 450,
+        subName: 'Eterna',
+        cardImg:'img/populares/Photo Menu-5.png',
+       },
+    
+       {
+        id: 5,
+        name: 'Picantovich',
+        precio: 750,
+        subName:'Pica 2 Veces',
+        cardImg:'img/populares/Photo Menu-6.png',
+       },
+    
+       {
+        id: 6,
+        name:'La Hasbulla',
+        precio: 990,
+        subName:'En honor al 1!',
+        cardImg:'img/populares/Photo Menu-8.png',
+    
+       },
+    
+       {
+        id: 7,
+        name:'Leo Messi',
+        precio: 10,
+        subName:'De pié señores!',
+        cardImg:'img/populares/Photo Menu-7.png',
+    
+       },
+    
+       {
+        id: 8,
+        name: 'Nick Gi',
+        precio: 'Gratis',
+        subName:'La que desaparece',
+        cardImg:'img/populares/Photo Menu-3.png',
+    
+       },
+    
+       {
+        id: 9,
+        name:'Bennazianna',
+        precio: 3650,
+        subName:'La mas completa',
+        cardImg:'img/recomendados/Photo Menu.png',
+       },
+    
+       {
+        id: 10,
+        name:'Tronco Pizza',
+        precio: 870,
+        subName:'Para todo el dia',
+        cardImg:'img/recomendados/Photo Menu-1.png',
+       },
+    
+       {
+        id: 11,
+        name:'Papas/Provensal',
+        precio: 360,
+        subName:'Van como piña',
+        cardImg:'img/recomendados/Photo Menu-2.png',
+       },
+    ];
+    
+    
