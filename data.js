5ab205b026fd1330ee5fa70032ffd48b03843b55
cespedesjuanc <cespedesjuanc@MacBook-Pro-de-Juan.local>
2022-09-14 00:16:21 -0300

    se renderiza productos desde JS

--- data.js
+++ data.js
@@ -1,95 +1,118 @@
 const productsData = [
-    {
-        id: 1,
-        name: 'La Mr. Pit',
-        precio: 350,
-        subName:'Solo para expertos',
-        category:'pizza',
-        cardImg:'./assets/img/populares/Photo_Menu_1.png',
-    },
-    {
-        id: 2,
-        name: '¡Q`Jamone!',
-        precio: 350,
-        subName:'c/jamón crudo',
-        category:'pizza',
-        cardImg:'./assets/img/populares/Photo_Menu_2.png',
-    },
-       {
-        id: 3,
-        name:'La Charly García',
-        precio: 380,
-        subName:'BASTA',
-        category:'pizza',
-        cardImg: './assets/img/populares/Photo_Menu_3.png',
-       },
-    
-       {
-        id: 4,
-        name:'La Maradona',
-        precio: 450,
-        subName: 'Eterna',
-        cardImg:'./assets/img/populares/Photo_Menu_4.png',
-       },
-    
-       {
-        id: 5,
-        name: 'Picantovich',
-        precio: 750,
-        subName:'Pica 2 Veces',
-        cardImg:'./assets/img/populares/Photo_Menu_5.png',
-       },
-    
-       {
-        id: 6,
-        name:'La Hasbulla',
-        precio: 990,
-        subName:'En honor al 1!',
-        cardImg:'./assets/img/populares/Photo_Menu_6.png',
-    
-       },
-    
-       {
-        id: 7,
-        name:'Leo Messi',
-        precio: 10,
-        subName:'De pié señores!',
-        cardImg:'./assets/img/populares/Photo_Menu_7.png',
-    
-       },
-    
-       {
-        id: 8,
-        name: 'Nick Gi',
-        precio: 'Gratis',
-        subName:'La que desaparece',
-        cardImg:'./assets/img/populares/Photo_Menu_8.png',
-    
-       },
-    
-       {
-        id: 9,
-        name:'Bennazianna',
-        precio: 3650,
-        subName:'La mas completa',
-        cardImg:'./assets/img/recomendados/Photo_Menu_1.png',
-       },
-    
-       {
-        id: 10,
-        name:'Tronco Pizza',
-        precio: 870,
-        subName:'Para todo el dia',
-        cardImg:'./assets/img/recomendados/Photo_Menu_2.png',
-       },
-    
-       {
-        id: 11,
-        name:'Papas/Provensal',
-        precio: 360,
-        subName:'Van como piña',
-        cardImg:'./assets/img/recomendados/Photo_Menu_3.png',
-       },
-    ];
-    
-    
+  {
+    id: 1,
+    name: "La Mr. Pit",
+    precio: 350,
+    subName: "Solo para expertos",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_1.png",
+  },
+  {
+    id: 2,
+    name: "¡Q`Jamone!",
+    precio: 350,
+    subName: "c/jamón crudo",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_2.png",
+  },
+  {
+    id: 3,
+    name: "La Charly García",
+    precio: 380,
+    subName: "BASTA",
+    category: "Mexican Food",
+    cardImg: "./assets/img/populares/Photo_Menu_3.png",
+  },
+
+  {
+    id: 4,
+    name: "La Maradona",
+    precio: 450,
+    subName: "Eterna",
+    category: "Mexican Food",
+    cardImg: "./assets/img/populares/Photo_Menu_4.png",
+  },
+
+  {
+    id: 5,
+    name: "Picantovich",
+    precio: 750,
+    subName: "Pica 2 Veces",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_5.png",
+  },
+
+  {
+    id: 6,
+    name: "La Hasbulla",
+    precio: 990,
+    subName: "En honor al 1!",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_6.png",
+  },
+
+  {
+    id: 7,
+    name: "Leo Messi",
+    precio: 10,
+    subName: "De pié señores!",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_7.png",
+  },
+
+  {
+    id: 8,
+    name: "Nick Gi",
+    precio: "Gratis",
+    subName: "La que desaparece",
+    category: "Wraps",
+    cardImg: "./assets/img/populares/Photo_Menu_8.png",
+  },
+
+  {
+    id: 9,
+    name: "Bennazianna",
+    precio: 3650,
+    subName: "La mas completa",
+    category: "Mexican Food",
+    cardImg: "./assets/img/populares/Photo_Menu_9.jpg",
+  },
+
+  {
+    id: 10,
+    name: "Tronco Pizza",
+    precio: 870,
+    subName: "Para todo el dia",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_10.jpeg",
+  },
+
+  {
+    id: 11,
+    name: "Papas | Provenzal",
+    precio: 360,
+    subName: "Van como piña",
+    category: "Napapuki",
+    cardImg: "./assets/img/populares/Photo_Menu_11.jpeg",
+  },
+  {
+    id: 11,
+    name: "Pizza Imperio",
+    precio: 560,
+    subName: "La de Carlitos",
+    category: "pizzas",
+    cardImg: "./assets/img/populares/Photo_Menu_12.jpg",
+  },
+];
+
+function splitProducts(size) {
+  let chunk = [];
+  for (let i = 0; i < productsData.length; i += size)
+    chunk.push(productsData.slice(i, i + size));
+  return chunk;
+}
+const allProducts = {
+  productList: splitProducts(3),
+  next: 1,
+  limit: splitProducts(3).length,
+};
